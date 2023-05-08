8558b0d19a1e032731704a734d7eee0ee8d340a5
victorchiaka <chiakavictor205@gmail.com>
2023-05-08 18:03:55 +0100

    to be modified

---
+++ Js/main.js
@@ -0,0 +1,8 @@
+const toTop = document.querySelector(".go-top");
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
